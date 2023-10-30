b62fcd2bd06c1a0e465996c33388937be662a753
Dylan Cianci <cianci.dy@gmail.com>
2023-10-30 18:27:59 +0100

    création db v.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_30_172716) do
+  create_table "contact_forms", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "localisation"
+    t.string "project_type"
+    t.integer "budget"
+    t.string "step_project"
+    t.string "email"
+    t.text "message"
+    t.string "phone_number"
+    t.date "desired_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
